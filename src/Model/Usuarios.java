f66f07a7675dd68f9e55365b2f0b9312c656a13a
AndyBardales17cm <andymaster1222@gmail.com>
2025-05-21 11:26:46 -0500

    Agregamos la clase USUARIOS

---
+++ src/Model/Usuarios.java
@@ -0,0 +1,27 @@
+package Model;
+
+public class Usuarios {
+    private String nombre; 
+    private String contraseña;
+    
+    public Usuarios(String nombre, String contraseña) {
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contaseña) {
+        this.contraseña = contaseña;
+    }    
+}
